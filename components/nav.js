ec9295c2e872e1b729c99a28f9d6179a738c73de
tbohn2 <tanner.bohn@yahoo.com>
2023-07-17 21:59:02 -0700

    Created nav

---
+++ components/nav.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function Nav({ currentPage, changePage }) {
+    return (
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a
+                    href="#AboutMe"
+                    onClick={() => changePage('AboutMe')}
+                    className={currentPage === 'AboutMe' ? 'nav-link active' : 'nav-link'}
+                >
+                    About Me
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Contact"
+                    onClick={() => changePage('Contact')}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Portfolio"
+                    onClick={() => changePage('Portfolio')}
+                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </a>
+            </li>
+            <li className="nav-item">
+                <a
+                    href="#Resume"
+                    onClick={() => changePage('Resume')}
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </a>
+            </li>
+        </ul>
+    );
+}
+
+export default Nav;
