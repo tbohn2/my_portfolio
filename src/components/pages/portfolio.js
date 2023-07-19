cef10dfcb60eb81250b52bb360dc241afb582971
tbohn2 <tanner.bohn@yahoo.com>
2023-07-19 11:52:02 -0700

    Created cards for application displays

--- src/components/pages/portfolio.js
+++ src/components/pages/portfolio.js
@@ -3,8 +3,62 @@ import React from 'react';
 
 export default function AboutMe() {
     return (
-        <section>
-            Hello
+        <section className='mt-5 col-10 d-flex flex-wrap justify-content-evenly'>
+            <div class="card col-3 mx-2 mb-5 bg-secondary">
+                <img src="https://user-images.githubusercontent.com/124842865/231046542-e3912b55-6db1-4bf3-b355-b9475cee8269.png" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Country Information and Currency Calulator</h5>
+                    <p class="card-text">JavaScript, Restful APIs</p>
+                    <a href="https://tbohn2.github.io/Country-info-and-exchange-rates/" class="btn btn-dark ms-3">Lauch App</a>
+                    <a href="" class="btn btn-dark ms-5">Visit GitHub Repository</a>
+                </div>
+            </div>
+            <div class="card col-3 mx-2 mb-5 bg-secondary">
+                <img src="https://user-images.githubusercontent.com/124842865/231046542-e3912b55-6db1-4bf3-b355-b9475cee8269.png" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Country Information and Currency Calulator</h5>
+                    <p class="card-text">JavaScript, Restful APIs</p>
+                    <a href="https://tbohn2.github.io/Country-info-and-exchange-rates/" class="btn btn-dark ms-3">Lauch App</a>
+                    <a href="" class="btn btn-dark ms-5">Visit GitHub Repository</a>
+                </div>
+            </div>
+            <div class="card col-3 mx-2 mb-5 bg-secondary">
+                <img src="https://user-images.githubusercontent.com/124842865/231046542-e3912b55-6db1-4bf3-b355-b9475cee8269.png" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Country Information and Currency Calulator</h5>
+                    <p class="card-text">JavaScript, Restful APIs</p>
+                    <a href="https://tbohn2.github.io/Country-info-and-exchange-rates/" class="btn btn-dark ms-3">Lauch App</a>
+                    <a href="" class="btn btn-dark ms-5">Visit GitHub Repository</a>
+                </div>
+            </div>
+            <div class="card col-3 mx-2 mb-5 bg-secondary">
+                <img src="https://user-images.githubusercontent.com/124842865/231046542-e3912b55-6db1-4bf3-b355-b9475cee8269.png" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Country Information and Currency Calulator</h5>
+                    <p class="card-text">JavaScript, Restful APIs</p>
+                    <a href="https://tbohn2.github.io/Country-info-and-exchange-rates/" class="btn btn-dark ms-3">Lauch App</a>
+                    <a href="" class="btn btn-dark ms-5">Visit GitHub Repository</a>
+                </div>
+            </div>
+            <div class="card col-3 mx-2 mb-5 bg-secondary">
+                <img src="https://user-images.githubusercontent.com/124842865/231046542-e3912b55-6db1-4bf3-b355-b9475cee8269.png" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Country Information and Currency Calulator</h5>
+                    <p class="card-text">JavaScript, Restful APIs</p>
+                    <a href="https://tbohn2.github.io/Country-info-and-exchange-rates/" class="btn btn-dark ms-3">Lauch App</a>
+                    <a href="" class="btn btn-dark ms-5">Visit GitHub Repository</a>
+                </div>
+            </div>
+            <div class="card col-3 mx-2 mb-5 bg-secondary">
+                <img src="https://user-images.githubusercontent.com/124842865/231046542-e3912b55-6db1-4bf3-b355-b9475cee8269.png" class="card-img-top" alt="..."></img>
+                <div class="card-body">
+                    <h5 class="card-title">Country Information and Currency Calulator</h5>
+                    <p class="card-text">JavaScript, Restful APIs</p>
+                    <a href="https://tbohn2.github.io/Country-info-and-exchange-rates/" class="btn btn-dark ms-3">Lauch App</a>
+                    <a href="" class="btn btn-dark ms-5">Visit GitHub Repository</a>
+                </div>
+            </div>
+
         </section>
     );
 }
