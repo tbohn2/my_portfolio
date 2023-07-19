2cbbdb33c836ebe6cc48a9aa30c3f4d6e57f12c1
tbohn2 <tanner.bohn@yahoo.com>
2023-07-19 09:12:38 -0700

    Moved components into src folder

---
+++ src/components/pages/aboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function AboutMe() {
+    return (
+        <section>
+            <div>
+                <h1 className='navbar' >Tanner Bohn</h1>
+                <p >I am a cool guy</p>
+            </div>
+        </section>
+    );
+}
