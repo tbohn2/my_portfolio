43edf0f05b2995f3de03f0016625768d27cd19d8
tbohn2 <tanner.bohn@yahoo.com>
2023-07-22 21:19:49 -0700

    Created objects for each project and mapped over

---
+++ src/components/project.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../styles/portfolio.css'
+
+export default function Project({ name, languages, imgURL, siteURL, gitURL }) {
+    return (
+        <div class="card col-3 mx-2 mb-5 bg-secondary">
+            <div className='myImgBox'>
+                <img src={imgURL} class="myProjImg" alt="..."></img>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">{name}</h5>
+                <p class="card-text">{languages}</p>
+                <a href={siteURL} class="btn btn-dark ">Lauch App</a>
+                <a href={gitURL} class="btn btn-dark ms-2">GitHub Repo</a>
+            </div>
+        </div>
+    )
+}
