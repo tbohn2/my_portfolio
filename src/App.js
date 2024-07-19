5082156bd955444cadaa696930834343fc683222
tbohn2 <tanner.bohn@yahoo.com>
2024-07-19 12:36:41 -0700

    Changed file structure to be compatible with vite + react instead of CRA

--- src/App.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Container from "./components/container";
-
-// Returns Container component
-const App = () => <Container />;
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Container from "./components/container";
+
+function App() {
+  return (
+    <Container />
+  )
+}
+
+export default App
