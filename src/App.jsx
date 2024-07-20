60f6df446c0eeb29938d1040d89f4dfa09a02e32
tbohn2 <tanner.bohn@yahoo.com>
2024-07-19 18:26:17 -0700

    Incorporated react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Container from "./components/container";
+import './styles/app.css';
 
 function App() {
   return (
-    <Container />
+    <Router basename='/my_portfolio'>
+      <Routes>
+        <Route path="/" element={<Container />} />
+      </Routes>
+    </Router>
   )
 }
 
