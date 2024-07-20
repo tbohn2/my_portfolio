a1ae4e9b8e122362e1b21ccd23950d99c3c4e47e
tbohn2 <tanner.bohn@yahoo.com>
2024-07-19 18:27:18 -0700

    Changed background and layout of about me page

--- src/components/pages/aboutMe.jsx
+++ src/components/pages/aboutMe.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import '../../styles/aboutMe.css'
-import pic from '../../assets/IMG_7980.jpg'
+import pic from '../../assets/portrait.jpg'
 
 // Returns About Me page
 export default function AboutMe() {
     return (
-        <section className="aboutMeBody col-8 my-5 d-flex">
-            <div className="leftContainer col-7 d-flex flex-column justify-content-around">
-                <h1 className="name text-light text-wrap">Tanner Bohn</h1>
-                <p className="text-light text-wrap">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, blanditiis neque. Maiores quasi quaerat explicabo saepe nesciunt. Animi suscipit iure nemo. Quae accusamus neque laudantium ab provident aperiam dolor. Dolores.</p>
-                <p className="text-light text-wrap">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, blanditiis neque. Maiores quasi quaerat explicabo saepe nesciunt. Animi suscipit iure nemo. Quae accusamus neque laudantium ab provident aperiam dolor. Dolores.</p>
+        <section id="aboutMeBody" className='d-flex justify-content-evenly col-10 pt-5'>
+            <div id='home-left' className="col-5 d-flex flex-column justify-content-evenly pt-5 px-3">
+                <h1 className="name">Hi, I'm <span className='fw-normal text-light'>Tanner</span></h1>
+                <h2 className="">FULL-STACK WEB DEVELOPER</h2>
+                <p className='text-light fs-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa dolor quidem magni provident rerum. Odio nulla illo, voluptates soluta, placeat fuga cumque autem, aperiam quos ratione exercitationem excepturi? Maiores, velit!</p>
             </div>
-            <div className='col-5 d-flex justify-content-center align-items-center'>
-                <div className='col-10 myPortraitBox'>
-                    <img src={pic} alt='' className='myImg'></img>
-                </div>
-            </div>
+            <img src={pic} alt='' className='myImg'></img>
         </section>
     );
 }
