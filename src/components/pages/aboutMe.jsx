14b114978058a73751a0d56e8dec675282e5d6b9
tbohn2 <tanner.bohn@yahoo.com>
2024-07-26 16:16:05 -0700

    Changed layout of home page

--- src/components/pages/aboutMe.jsx
+++ src/components/pages/aboutMe.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import '../../styles/aboutMe.css'
-import pic from '../../assets/portrait.jpg'
+import pic from '../../assets/portrait2.png'
 
 // Returns About Me page
 export default function AboutMe() {
     return (
-        <section id="aboutMeBody" className='d-flex justify-content-evenly col-10 pt-5'>
-            <div id='home-left' className="col-5 d-flex flex-column justify-content-evenly pt-5 px-3">
-                <h1 className="name">Hi, I'm <span className='fw-normal text-light'>Tanner</span></h1>
+        <section id="aboutMeBody" className='d-flex justify-content-between roboto col-11 pt-5'>
+            <div id='home-left' className="col-6 d-flex flex-column">
+                <h1 className="">Hi, I'm <span>Tanner</span></h1>
                 <h2 className="">FULL-STACK WEB DEVELOPER</h2>
                 <p className='text-light fs-5'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa dolor quidem magni provident rerum. Odio nulla illo, voluptates soluta, placeat fuga cumque autem, aperiam quos ratione exercitationem excepturi? Maiores, velit!</p>
+                <img src={pic} alt='' className='myImg align-self-center'></img>
             </div>
-            <img src={pic} alt='' className='myImg'></img>
         </section>
     );
 }
