989853a989fbf40bfa0cce098920614fdd793ea3
tbohn2 <tanner.bohn@yahoo.com>
2024-07-31 21:59:30 -0700

    Changed layout of header; added logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,31 +1,32 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import '../styles/header.css'
+import logo from '../assets/logo.png'
 
 // Returns nav at top of each page
 function Header({ displayPage, setDisplayPage }) {
     return (
-        <header className='col-12 d-flex'>
-            <h1 className='col-4 text-light fw-bold text-center'>Tanner Bohn</h1>
+        <header className='col-12 ps-5 d-flex justify-content-between'>
+            <img src={logo} alt="Logo" className='col-1' />
             <ul className="myNav roboto fw-lighter col-8 d-flex justify-content-evenly align-items-center">
                 <li>
                     <Link to="/" onClick={() => setDisplayPage('AboutMe')} className={displayPage === 'AboutMe' ? ' active' : 'inactive'}>
-                        Home
+                        home
                     </Link>
                 </li>
                 <li>
                     <Link to="/portfolio" onClick={() => setDisplayPage('Portfolio')} className={displayPage === 'Portfolio' ? ' active' : 'inactive'}                >
-                        Portfolio
+                        portfolio
                     </Link>
                 </li>
                 <li>
                     <Link to="/resume" onClick={() => setDisplayPage('Resume')} className={displayPage === 'Resume' ? ' active' : 'inactive'}                >
-                        Resume
+                        resume
                     </Link>
                 </li>
                 <li>
                     <Link to="/contact" onClick={() => setDisplayPage('Contact')} className={displayPage === 'Contact' ? ' active' : 'inactive'}                >
-                        Contact
+                        contact
                     </Link>
                 </li>
             </ul>
