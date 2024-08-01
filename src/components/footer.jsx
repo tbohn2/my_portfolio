c5be2a3743549f3a6da8af71ef601995e7643563
tbohn2 <tanner.bohn@yahoo.com>
2024-07-31 19:35:25 -0700

    Added Linked-In and GitHub logos to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import '../styles/footer.css'
+import gitLogo from '../assets/github-logo.png'
+import liLogo from '../assets/LI-logo.png'
 
-// Returns footer including buttons with links to GitHub, LinkedIn, and Facebook
+// Returns footer including buttons with as to GitHub, aedIn, and Facebook
 function Footer() {
     return (
-        <div className="footerBox border-top border-light col-8 d-flex justify-content-evenly align-items-center">
-            <a href="https://github.com/tbohn2" type='button' className="btn btn-lg footerBtn btn-outline-light text-light ">
-                GitHub
-            </a>
-            <a href="https://www.linkedin.com/in/tanner-bohn-084060285" type='button' className="btn btn-lg footerBtn btn-outline-light text-light ">
-                LinkedIn
-            </a>
-            <a href="https://www.facebook.com/tanner.bohn.92" type='button' className="btn btn-lg footerBtn btn-outline-light text-light ">
-                Facebook
-            </a>
-
-        </div>
+        <footer className="roboto col-12 px-5 d-flex justify-content-between align-items-center">
+            <p>tanner.bohn@yahoo.com</p>
+            <div className="d-flex justify-content-between col-1">
+                <a href="https://www.aedin.com/in/tanner-bohn-084060285" type='button' className="">
+                    <img src={liLogo} alt="aed-in Logo" />
+                </a>
+                <a href="https://github.com/tbohn2" type='button' className="">
+                    <img src={gitLogo} alt="aed-in Logo" />
+                </a>
+            </div>
+        </footer>
 
     )
 }
