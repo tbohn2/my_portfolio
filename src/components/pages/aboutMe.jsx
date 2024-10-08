4c9a5bcbf34822335dd0111ed9f772464b44c419
tbohn2 <tanner.bohn@yahoo.com>
2024-10-07 17:46:22 -0700

    Changed photo and spacing on about me page

--- src/components/pages/aboutMe.jsx
+++ src/components/pages/aboutMe.jsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/aboutMe.css'
-import pic from '../../assets/portrait2.png'
+import pic from '../../assets/portrait4.jpg'
 
-export default function AboutMe({ mobile, setDisplayPage }) {
+export default function AboutMe({ setDisplayPage }) {
+
+    const [medScreen, setmedScreen] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth <= 768) {
+            setmedScreen(true);
+        }
+        window.addEventListener('resize', () => {
+            if (window.innerWidth <= 992) {
+                setmedScreen(true);
+            } else {
+                setmedScreen(false);
+            }
+        });
+    }, []);
 
     return (
         <section id="aboutMeBody" className='fade-in roboto'>
             <div id='aboutMeContent' className='col-12 d-flex justify-content-center align-items-start'>
-                <div id="aboutText" className='col-md-7 col-11 me-3'>
-                    <h1 className="my-0 work-sans">Tanner Bohn</h1>
-                    {mobile &&
+                <div id="aboutText" className='col-md-7 col-11'>
+                    <h1 className="work-sans">Tanner Bohn</h1>
+                    {medScreen &&
                         <img src={pic} alt='' className='myImg my-3 align-self-center'></img>
                     }
-                    <h2 className="">FULL-STACK WEB DEVELOPER</h2>
+                    <h2 className="my-4">FULL-STACK WEB DEVELOPER</h2>
                     <p className='text-light fs-5 col-12'>
                         I bring ideas to life through clean code and responsive design.
                         Whether it's creating interactive interfaces or ensuring backend efficiency,
@@ -23,7 +38,7 @@ export default function AboutMe({ mobile, setDisplayPage }) {
                         Feel free to explore my <Link to='/portfolio' onClick={() => setDisplayPage('Portfolio')}>portfolio</Link> and <Link to='/contact' onClick={() => setDisplayPage('Contact')}>get in touch</Link> for collaborations or inquiries!
                     </p>
                 </div>
-                {!mobile &&
+                {!medScreen &&
                     <img src={pic} alt='' className='myImg ms-3'></img>
                 }
             </div>
