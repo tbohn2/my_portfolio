aa9f02503980b12a2575313d7b597002098f5a0f
Tanner Bohn <tanner.bohn@truneighbor.co>
2025-11-14 15:19:32 -0700

    Changed layout/wording of homepage

--- src/components/pages/aboutMe.jsx
+++ src/components/pages/aboutMe.jsx
@@ -22,25 +22,26 @@ export default function AboutMe({ setDisplayPage, handleLoadedPage }) {
 
     return (
         <section id="aboutMeBody" className='fade-in roboto'>
-            <div id='aboutMeContent' className='col-12 d-flex justify-content-evenly align-items-start'>
+            <div id='aboutMeContent' className='col-12 d-flex justify-content-evenly align-items-center'>
+                {!medScreen &&
+                    <img src={pic} alt='' className='myImg me-4' onLoad={() => handleLoadedPage('AboutMe')}></img>
+                }
                 <div id="aboutText" className='col-md-7 col-10'>
-                    <h1 className="work-sans">Tanner Bohn</h1>
                     {medScreen &&
                         <img src={pic} alt='' className='myImg my-4 align-self-center' onLoad={() => handleLoadedPage('AboutMe')}></img>
                     }
-                    <h2 className="my-4">FULL-STACK WEB DEVELOPER</h2>
+                    <h1 className="work-sans">Tanner Bohn</h1>
+                    <h2 className="my-4">FULL-STACK DEVELOPER</h2>
                     <p className='text-light fs-5 col-12'>
-                        I bring ideas to life through clean code and responsive design.
-                        Whether it's creating interactive interfaces or ensuring backend efficiency,
-                        I strive to deliver solutions that balance performance with creativity.
+                        I transform concepts into reality through clean code and intuitive design.
+                        From crafting engaging user experiences to building robust server-side systems,
+                        I focus on creating solutions that seamlessly blend innovation with reliability.
                     </p>
-                    <p className='text-light fs-5 col-12'>
-                        Feel free to explore my <Link to='/portfolio' onClick={() => setDisplayPage('Portfolio')}>portfolio</Link> and <Link to='/contact' onClick={() => setDisplayPage('Contact')}>get in touch</Link> for collaborations or inquiries!
-                    </p>
+                    <div className='col-12 d-flex gap-3 mt-3'>
+                        <Link to='/portfolio' onClick={() => setDisplayPage('Portfolio')} className='btn btn-outline-primary btn-lg'>View Portfolio</Link>
+                        <Link to='/resume' onClick={() => setDisplayPage('Resume')} className='btn btn-outline-primary btn-lg'>View Resume</Link>
+                    </div>
                 </div>
-                {!medScreen &&
-                    <img src={pic} alt='' className='myImg ms-3' onLoad={() => handleLoadedPage('AboutMe')}></img>
-                }
             </div>
         </section>
     );
